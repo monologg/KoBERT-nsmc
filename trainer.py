49c66a97c9b9db045680bb62e4c8e2c779bc183b
monologg <adieujw@gmail.com>
2020-05-11 00:40:15 +0900

    Fix the seed issue

--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from transformers import AdamW, get_linear_schedule_with_warmup
 
-from utils import set_seed, compute_metrics, get_label, MODEL_CLASSES
+from utils import compute_metrics, get_label, MODEL_CLASSES
 
 logger = logging.getLogger(__name__)
 
@@ -70,7 +70,6 @@ class Trainer(object):
         self.model.zero_grad()
 
         train_iterator = trange(int(self.args.num_train_epochs), desc="Epoch")
-        set_seed(self.args)
 
         for _ in train_iterator:
             epoch_iterator = tqdm(train_dataloader, desc="Iteration")
